aea4ca1ebdda3d32acd937686995aeb659c2ec50
nickslick03 <nicholasepps03@gmail.com>
2024-09-11 10:53:28 -0400

    build release

--- wiinote-form/wiimote form/obj/Release/net8.0-windows/wiinote-form.AssemblyInfo.cs
+++ wiinote-form/wiimote form/obj/Release/net8.0-windows/wiinote-form.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("wiinote-form")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7400072211fb9d86ea7d1c27e51a33c9f2cc72c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("wiinote-form")]
--- wiinote.ps1
+++
@@ -1,5 +0,0 @@
-Start-Process -FilePath ".\wiimote form\wiimote form\bin\Debug\net8.0-windows\wiimote form.exe"
-cd wiinote-webpage
-Start-Process "http://localhost:4173/"
-npm run preview
-
